c42273417017df886971d97fa30501fd4c3eed8a
gbonillo463 <gbonillo@uoc.edu>
2026-01-18 17:34:09 +0100

    ej 4b2 COMPLETADO

--- 4b/ej4b2.py
+++ 4b/ej4b2.py
@@ -22,51 +22,29 @@ Ejemplo:
         8.0
         8.0        
         
-Enunciat:
-
-Implementa la funció 'average_score_ram(scores_dict)' que rep un
-diccionari amb el nom i la qualificació dels estudiants i calcula
-la mitjana de qualificacions. Cal utilitzar un bucle 'for' per il·lustrar
-l'ús de RAM.
-
-La funció 'average_score_heap(scores_dict)' ja està implementada i ha d'emmagatzemar
-la mitjana de qualificacions al Heap; no obstant això, hi ha un error en aquest càlcul 
-identifica'l i corregeix-lo.
-
-Paràmetres:
-- scores_dict: Diccionari amb el nom de l'estudiant com a clau i la seva
-qualificació com a valor.
-
-Exemple:
-     Entrada:
-         average_score_ram({'Juan': 8, 'Maria': 7, 'Pedro': 9})
-         average_score_heap({'Juan': 8, 'Maria': 7, 'Pedro': 9})
-        
-     Sortida:
-         8.0
-         8.0
-    
 """
 
 
 def average_score_ram(scores_dict):
-    # Store the dictionary in RAM
-    # Write here your code
-    pass
+    average_score = 0
+    for score in scores_dict:
+        average_score += scores_dict[score] / len(scores_dict)
+    return round(average_score, 2)
+
 
 
 def average_score_heap(scores_dict):
     # Store the dictionary in Heap    
     score_list = list(scores_dict.values())
     # You should correct and overwrite something in the following line.
-    heap_average = sum(score_list) 
+    heap_average = sum(score_list) / len(score_list)
     return heap_average
 
 
 # Si quieres probar tu código, descomenta las siguientes líneas y ejecuta el script
 # Si vols provar el teu codi, descomenta les línies següents i executa l'script
 
-# scores_dict = {"Juan": 6.7, "Maria": 9.1, "Pedro": 6.5, "Tomas": 8.2, "Julio": 9}
+scores_dict = {"Juan": 6.7, "Maria": 9.1, "Pedro": 6.5, "Tomas": 8.2, "Julio": 9}
 
-# print(average_score_ram(scores_dict))
-# print(average_score_heap(scores_dict))
+print(average_score_ram(scores_dict))
+print(average_score_heap(scores_dict))
