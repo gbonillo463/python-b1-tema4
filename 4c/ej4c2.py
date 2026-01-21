39c011a236818ae3e1e7239c73b68ba5bc5d0517
gbonillo463 <spamgerard4@gmail.com>
2026-01-21 20:21:42 +0100

    ej 4c2 COMPLETADO

--- 4c/ej4c2.py
+++ 4c/ej4c2.py
@@ -21,55 +21,31 @@ Ejemplo:
         Driving a car
         Riding a bicycle
 
-
-Enunciat:
-Es demana crear una interfície "Vehicles" que tingui un mètode abstracte "drive".
-A més, cal crear les classes concretes "Car" i "Bicycle" que implementin
-la interfície "Vehicles".
-
-El mètode "drive" ha d'imprimir "Driving a car" per a la classe "Car" i "Riding"
-a bicycle" per a la classe "Bicycle".
-
-Paràmetres:
-     La classe Car i Bicycle no reben paràmetres.
-        
-Exemple:
-     Entrada:
-         car = Car()
-         print(car.drive())
-
-         bicycle = Bicycle()
-         print(bicycle.drive())
-     Sortida:
-         Driving a car
-         Riding a bicycle
 """
 
 from abc import ABC, abstractmethod
 
 # Write abstract class Vehicles here 
-class Vehicles():
+class Vehicles(ABC):
+    @abstractmethod
     def drive(self):
-        # Write here your code
         pass
 
 # Corret and overwrite class Car(Vehicles) here 
-class Car():
+class Car(Vehicles):
     def drive(self):
-        # Write here your code
-        pass
+        return "Driving a car"
 
 # Corret and overwrite class Bicycle(Vehicles) here 
-class Bicycle():
+class Bicycle(Vehicles):
     def drive(self):
-        # Write here your code
-        pass
+        return "Riding a bicycle"
 
 
 # Si quieres probar tu código, descomenta las siguientes líneas y ejecuta el script
 # Si vols provar el teu codi, descomenta les línies següents i executa l'script
-# car = Car()
-# print(car.drive())
+car = Car()
+print(car.drive())
 
-# bicycle = Bicycle()
-# print(bicycle.drive())
+bicycle = Bicycle()
+print(bicycle.drive())
