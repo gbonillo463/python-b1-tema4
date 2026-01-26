997cd180914a5a1faa13b1484034504ea7f6195c
gbonillo463 <spamgerard4@gmail.com>
2026-01-26 13:05:27 +0100

    ej 4d2 COMPLETADO

--- 4d/ej4d2.py
+++ 4d/ej4d2.py
@@ -17,31 +17,17 @@ Ejemplo:
         Perez
         30
 
-Enunciat:
-Implementa la funció 'create_read_file()', que no rep cap
-paràmetre pel fet que aquesta funció ha de crear un fitxer de text
-'text_file.txt', dins d'aquest fitxer cal escriure tres línies d'
-informació. La primera línia ha de contenir un nom, la segona línia un
-cognom i, finalment, l'edat. A continuació cal llegir el fitxer i imprimir
-per consola totes les línies del mateix.
-
-Paràmetre:
-- No rep cap paràmetre pel fet que dins d'aquesta funció es crea un
-fitxer de text.
-
-Exemple:
-     Sortida:
-         Joan
-         Perez
-         30
-
 """
 def create_read_file():
-    # Write here your code
-    pass
-
+    with open('text_file.txt', 'w') as f:
+        f.write('Juan')
+        f.write('\nPerez')
+        f.write('\n30')
+    
+    with open('text_file.txt', 'r') as t:
+        print(t.read())
 
 
 # Si quieres probar tu código, descomenta las siguientes líneas y ejecuta el script
 # Si vols provar el teu codi, descomenta les línies següents i executa l'script
-# create_read_file()
+create_read_file()
