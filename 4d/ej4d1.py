6b2085c475ab3c7b6a8bb29b74bf51c6a64e6bd0
gbonillo463 <spamgerard4@gmail.com>
2026-01-26 12:44:00 +0100

    ej 4d1 COMPLETADO

--- 4d/ej4d1.py
+++ 4d/ej4d1.py
@@ -15,30 +15,15 @@ Ejemplo:
 
     Salida:
         "Result: " 11
-
-Enunciat:
-Realitzant l'entrada per consola de les dades, implementa la funció 'sum'
-que sol·liciti l'entrada de dos números amb 'input' i torni la suma dels
-números.
-
-Paràmetre:5
-No rep cap paràmetre pel fet que dins de la funció se sol·licita la
-entrada dels números.
-
-Exemple:
-     Entrada:
-         "Insert the first number: " 8
-         "Insert the second number: " 3
-
-     Sortida:
-         "Result: " 11
 """
 
 def sum():
-    # Write here your code
-    pass
-
+    number_1 = int(input("Insert the first number: "))
+    number_2 = int(input("Insert the second number: "))
+    result = number_1 + number_2
+    print("Result: ", result)
+    return result
 
 # Si quieres probar tu código, descomenta las siguientes líneas y ejecuta el script
 # Si vols provar el teu codi, descomenta les línies següents i executa l'script
-# sum()
+sum()
