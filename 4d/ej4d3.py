1d4ffacd5676d5151607508a657d7bb661c64d3b
gbonillo463 <spamgerard4@gmail.com>
2026-01-26 13:13:56 +0100

    ej 4d3 COMPLETADO

--- 4d/ej4d3.py
+++ 4d/ej4d3.py
@@ -23,39 +23,16 @@ Ejemplo:
         Julio
         30
 
-Enunciat:
-
-Implementa una funció 'read_and_write', no rep cap paràmetre a causa de
-que, dins de la mateixa cal sol·licitar l'entrada de 2 dades mitjançant
-teclat.
-
-En el moment de sol·licitar l'ingrés de les dades s'ha de considerar el
-següent text.
-'Insert your name: ' El valor introduït ha de ser de tipus str.
-'Insert your age: ' El valor introduït ha de ser de tipus int.
-
-S'ha de crear un fitxer de text 'file.txt' on La informació entrada
-per consola s'ha de guardar en aquest fitxer i s'ha d'imprimir per consola
-des del fitxer de text.
-
-Paràmetre:
-No rep cap paràmetre.
-
-Exemple:
-     Entrada:
-         'Insert your name: ' Juliol
-         'Insert your age: ' 30
-     Sortida:
-         Juliol
-         30
-
 """
 
 def read_and_write():
-    # Write here your code
-    pass
+    name = str(input('Inster your name: '))
+    age = int(input('Inster your age: '))
 
-
+    with open('file.txt', 'w') as f:
+        f.write(f'{name}\n{age}\n')
+    with open('file.txt', 'r') as f:
+        print(f.read())
 # Si quieres probar tu código, descomenta las siguientes líneas y ejecuta el script
 # Si vols provar el teu codi, descomenta les línies següents i executa l'script
-# read_and_write()
+read_and_write()
